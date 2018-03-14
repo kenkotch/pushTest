a4be3be7874345dbb9728d66528f637f0f6f2dac
Ken Kotch <hello@ken.uno>
2018-03-14 10:41:31 -0600

    push notifications working on iOS!

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react'
+import FCM, { FCMEvent } from 'react-native-fcm'
 import {
-  Platform,
   Text,
   View
 } from 'react-native'
 
 export default class App extends Component {
+  componentDidMount() {
+    FCM.requestPermissions() // for iOS
+    FCM.getFCMToken().then(token => {
+      console.log(token)
+      // store fcm token in your server
+    })
+    this.notificationUnsubscribe = FCM.on(FCMEvent.Notification, (notif) => {
+      // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+    })
+    this.refreshUnsubscribe = FCM.on(FCMEvent.RefreshToken, (token) => {
+      console.log(token) // fcm token may not be available on first load, catch it here
+    })
+
+    FCM.subscribeToTopic('/topics/foo-bar')
+    FCM.unsubscribeFromTopic('/topics/foo-bar')
+  }
+  componentWillUnmount() {
+    // prevent leaking
+    this.refreshUnsubscribe()
+    this.notificationUnsubscribe()
+  }
+
   render() {
+    console.disableYellowBox = true
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
+      <View style={ styles.container }>
+        <Text style={ styles.welcome }>
           React Native
         </Text>
-        <Text style={styles.instructions}>
+        <Text style={ styles.instructions }>
           Firebase Push Test
         </Text>
       </View>
